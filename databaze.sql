cf287f9e9f41392ddb96e3bee466d190cf9fd0db
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-10-15 08:03:29 +0200

    Update databaze.sql

--- databaze.sql
+++ databaze.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE school; /* vytvoření databáze */
 DROP DATABASE school; /* mazání databáze */
-create table car ( 
+CREATE TABLE car ( 
  id_car int,
  manafacturer varchar(255), /* vytvoření tabulky */
  model varchar(255)
 );
-create table employees (
+CREATE TABLE employees (
  jméno varchar(255),
  příjmení varchar(255), /* přidání někoho nebo něco do tabulky */
  věk int,
@@ -14,35 +14,36 @@ create table employees (
 
 describe employees;
 
-alter table employees
+ALTER TABLE employees
 add column id_employee int FIRST; /* přidání řádku */
 change column vintage manafacturing_year smallint; /* změna názvu řádku */
 
-insert into employees
-values (2,'Walter','White','52','jesse.whereismymeth.com') /*zaměstnanec přidání do tabulky */
+INSERT INTO employees
+VALUES (2,'Walter','White','52','jesse.whereismymeth.com') /*zaměstnanec přidání do tabulky */
 
-select*
-from employees /* vypsání vše co je v tabulce */
+SELECT*
+FROM employees /* vypsání vše co je v tabulce */
 
 /* 30.09.2020 */
 
-drop table garages;
+DROP TABLE garages;
 
 /* 14.10.2020 */
 
-create table garages(
-id_garage int not null auto_increment, /* auto_increment zařazuje automaticky do tabulky*/ /* not_null nemusí se vypisovat číslo uděla automaticky pomocí prvního příkazu */
-adress varchar(255),
+CREATE TABLE garages(
+id_garage int not null auto_increment, 
 email varchar(255),
 telephone int,
-car VARCHAR(255),
-
-primary key (id_garage) /* primary key zakazuje duplikáty */
+car varchar(255),
+adress varchar(255)
 );
 
-insert into garages (adress, email, telephone, car,) /* specifikování auta a adresy a dalších věcí */
-values ('někde někdy 666', 'adress@adress.cz', '666 666 666', 'Ford Mustang');
+ALTER TABLE garages MODIFY COLUMN id_garage INT NOT NULL AUTO_INCREMENT PRIMARY KEY; /* auto_increment zařazuje automaticky do tabulky*/ /* Primary key zakáže duplikáty */ /* not_null nemusí se vypisovat číslo uděla automaticky pomocí prvního příkazu */
 
-alter table employees
-auto_increment = id_employee,
-add primary key (id_employee);
+describe garages;
+
+INSERT INTO garages 
+VALUES (null, 'Prčice', 'adress@adress.cz', '666666666', 'Ford Mustang' 
+); /* specifikování auta a adresy a dalších věcí */
+ 
+ALTER TABLE employees MODIFY COLUMN id_employee INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
