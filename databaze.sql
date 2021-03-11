39956c35754fbc94b47ab0b586052f58cc11399b
SimonSiksta420 <simon.siksta@student.ossp.cz>
2021-03-11 13:26:02 +0100

    Update databaze.sql

--- databaze.sql
+++ databaze.sql
@@ -282,3 +282,9 @@ VALUES ("1", "1", "1", "1", "1", "1")
 
 ALTER TABLE students
 ADD UNIQUE (email);
+
+/* 11.03.2021 */
+SELECT name, shortname, firstname, lastname FROM schedule sch
+JOIN subjects su ON sch.id_subject = su.id_subject
+JOIN classrooms cl ON sch.id_classroom = cl.id_classroom
+JOIN teachers te ON sch.id_teacher = te.id_teacher;
